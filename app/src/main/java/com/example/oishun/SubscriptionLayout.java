6b096dde98bc872e52b9703deb1480f07b4bca42
Khondker Bin Yamin <yamin.24csedu.029@gmail.com>
2019-09-18 00:27:40 +0600

    Adding listener to floating action button to HomePage

--- app/src/main/java/com/example/oishun/SubscriptionLayout.java
+++ app/src/main/java/com/example/oishun/SubscriptionLayout.java
@@ -1,6 +1,7 @@
 package com.example.oishun;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SubscriptionLayout extends Fragment  {
 
@@ -21,10 +24,11 @@ public class SubscriptionLayout extends Fragment  {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     public void addContentList(){
-        Context context = getActivity().getApplicationContext();
+        final Context context = getActivity().getApplicationContext();
         subscribedContentList = (ListView) view.findViewById(R.id.subscription_contents);
         contentNames = getResources().getStringArray(R.array.contentNames);
 
@@ -33,6 +37,16 @@ public class SubscriptionLayout extends Fragment  {
 
         CustomAdapter adapter = new CustomAdapter(context,contentNames,coverPhotos);
         subscribedContentList.setAdapter(adapter);
+        FloatingActionButton fab = view.findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent().setClass(context,VoiceReorder.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     @Nullable
