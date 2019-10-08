9f51d087e01929b1ab875661af783ab23ecb6f76
A.M. Aahad <45429535+viper4717@users.noreply.github.com>
2019-10-08 11:17:14 +0600

    Changed the layout design of sign-in, sign-up page

--- app/src/main/java/com/example/oishun/MainActivity.java
+++ app/src/main/java/com/example/oishun/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
