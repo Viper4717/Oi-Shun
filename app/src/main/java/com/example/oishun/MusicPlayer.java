9767d9ec1cf5d99e062deaed18650e91f81017b9
azminazran <52544035+azminazran@users.noreply.github.com>
2019-10-16 02:09:29 +0600

    Music player playing audio from firebase

--- app/src/main/java/com/example/oishun/MusicPlayer.java
+++ app/src/main/java/com/example/oishun/MusicPlayer.java
@@ -1,10 +1,14 @@
 package com.example.oishun;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -12,24 +16,39 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.IOException;
 
 
-public class MusicPlayer extends AppCompatActivity {
+public class MusicPlayer extends AppCompatActivity implements MediaPlayer.OnPreparedListener{
 
     ImageButton playButton;
     SeekBar seekBar;
     TextView elapsedTimeLabel;
     TextView remainingTimeLabel;
     MediaPlayer mp;
+    String playingURL;
+    String imageURL;
+    String recordingName;
     int totalTime;
-
+    final int REQUEST_PERMISSION_CODE = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //totalTime = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+
+        if(!checkPermissionFromDevice())
+            requestPermissionFromDevice();
 
         playButton = (ImageButton)findViewById(R.id.playButton);
         elapsedTimeLabel = (TextView) findViewById(R.id.elapsedTimeLabel);
@@ -39,29 +58,23 @@ public class MusicPlayer extends AppCompatActivity {
         //mp = MediaPlayer.create(this,R.raw.music);
         mp = new MediaPlayer();
         mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        String url = "https://firebasestorage.googleapis.com/v0/b/oishun-73200.appspot.com/o/Recordings%2Ftest%2Fabcd.mp3?alt=media&token=a3e7bd3e-f59c-441d-bcfe-566a4bf7a849";
-        try {
-            mp.setDataSource(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        fetchAudioUrlFromFirebase();
+        //totalTime = 0;
 
         mp.setLooping(true);
         mp.seekTo(0);
         mp.setVolume(0.5f, 0.5f);
-        totalTime = mp.getDuration();
 
-        Toast.makeText(this, ""+totalTime, Toast.LENGTH_SHORT).show();
+
+        //Toast.makeText(this, String.valueOf(totalTime), Toast.LENGTH_SHORT).show();
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!mp.isPlaying()) {
                     // Stopping
                     try {
-                        mp.prepare();
                         mp.start();
-                    } catch (IOException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
 
@@ -77,6 +90,69 @@ public class MusicPlayer extends AppCompatActivity {
 
         // Seek Bar
         seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+    }
+
+    private boolean checkPermissionFromDevice() {
+        int writeExternalStorageResult = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+        return writeExternalStorageResult == PackageManager.PERMISSION_GRANTED;
+    }
+
+    //Method to request write and record permission from device
+    private void requestPermissionFromDevice() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.INTERNET
+        }, REQUEST_PERMISSION_CODE);
+    }
+
+    private void fetchAudioUrlFromFirebase() {
+        final FirebaseStorage storage = FirebaseStorage.getInstance();
+        // Create a storage reference from our app
+        //Toast.makeText(MusicPlayer.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+        playingURL = "https://firebasestorage.googleapis.com/v0/b/oishun-73200.appspot.com/o/Recordings%2Ftest%2Fabcd.mp3?alt=media&token=a3e7bd3e-f59c-441d-bcfe-566a4bf7a849";
+        StorageReference storageRef = storage.getReferenceFromUrl(playingURL);
+        //Toast.makeText(MusicPlayer.this,storageRef.getDownloadUrl().toString(),Toast.LENGTH_LONG).show();
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            //Toast.makeText(MusicPlayer.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onSuccess(Uri uri) {
+                //Toast.makeText(MusicPlayer.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+                try {
+                    // Download url of file
+                    final String url = uri.toString();
+                    mp.setDataSource(url);
+                    // wait for media player to get prepare
+                    mp.setOnPreparedListener(MusicPlayer.this);
+                    mp.prepareAsync();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //totalTime = mp.getDuration();
+            }
+
+
+
+
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MusicPlayer.this, "Failure", Toast.LENGTH_SHORT).show();
+                        Log.i("TAG", e.getMessage());
+                    }
+                });
+
+    }
+
+    @Override
+    public void onPrepared(final MediaPlayer mp) {
+        totalTime = mp.getDuration();
+        mp.start();
+        // Seek Bar
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        //Toast.makeText(this, ""+String.valueOf(totalTime), Toast.LENGTH_SHORT).show();
         seekBar.setMax(totalTime);
         seekBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
@@ -100,7 +176,6 @@ public class MusicPlayer extends AppCompatActivity {
                 }
         );
 
-        // Thread (Update positionBar & timeLabel)
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -114,8 +189,6 @@ public class MusicPlayer extends AppCompatActivity {
                 }
             }
         }).start();
-
-
     }
 
     private Handler handler = new Handler() {
