9baddab26c3212a8ab6b9487afdf4db677417bc7
A.M. Aahad <45429535+viper4717@users.noreply.github.com>
2019-10-15 16:02:01 +0600

    Added additional ids and navigation drawer listener

--- app/src/main/java/com/example/oishun/HomePage.java
+++ app/src/main/java/com/example/oishun/HomePage.java
@@ -1,36 +1,45 @@
 package com.example.oishun;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ListView subscribedContentList;
     private String[] contentNames;
     private int[] coverPhotos = {R.drawable.music_icon_image};
     FloatingActionButton recordActionButton;
+    DrawerLayout drawerLayout;
+    NavigationView navigationBar;
     String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        setNavigationViewListener();
         Intent tempIntent = getIntent();
         userName = tempIntent.getStringExtra("user_name");
         //Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
 
         recordActionButton = findViewById(R.id.recordActionButton);
+        drawerLayout = findViewById(R.id.DrawerLayout);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
@@ -51,7 +60,30 @@ public class HomePage extends AppCompatActivity {
         //CustomAdapter adapter = new CustomAdapter(this,contentNames,coverPhotos);
         //subscribedContentList.setAdapter(adapter);
 
+    }
 
+    private void setNavigationViewListener() {
+        navigationBar = (NavigationView) findViewById(R.id.navigationBar);
+        navigationBar.setNavigationItemSelectedListener(this);
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        switch (menuItem.getItemId()) {
+            case R.id.userProfile: {
+                Intent intent = new Intent(HomePage.this, UserPage.class);
+                intent.putExtra("user_name", userName);
+                startActivity(intent);
+                break;
+            }
+            case R.id.signOut: {
+                finish();
+                break;
+            }
+        }
+        //close navigation drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/example/oishun/UserPage.java
+++ app/src/main/java/com/example/oishun/UserPage.java
@@ -8,20 +8,34 @@ package com.example.oishun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class UserPage extends AppCompatActivity {
 
     ListView personalContentView;
     String[] personalContentNames;
     private int[] coverPhotos = {R.drawable.music_icon_image};
+    ImageView userImage;
+    TextView userNameText;
+    Button subscribeButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+
+        userImage = (ImageView) findViewById(R.id.userImage);
+        userNameText = (TextView) findViewById(R.id.userNameText);
+        subscribeButton = (Button) findViewById(R.id.subscribeButton);
+        Intent tempIntent = getIntent();
+        String userName = tempIntent.getStringExtra("user_name");
+        userNameText.setText(userName);
 
         personalContentNames = getResources().getStringArray(R.array.contentNames);
 
